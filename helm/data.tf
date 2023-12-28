312c8c3d7fd57f59651d077758c7d406953d7201
root <root@OSH-NOTE>
2023-12-28 16:15:11 +0900

    aws-auth auto

--- helm/data.tf
+++ helm/data.tf
@@ -9,3 +9,12 @@ data "terraform_remote_state" "test" {
     }
   }
 }
+
+
+data "template_file" "aws-auth" {
+  template = file("${path.module}/templates/aws_auth.yaml.tpl")
+
+  vars = {
+    rolearn   = data.terraform_remote_state.test.outputs.node-role
+  }
+}
