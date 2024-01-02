e79003e23a4bd24fe9394437c64817d6f737c56f
odark <odark@naver.com>
2024-01-02 21:54:50 +0900

    variables update

--- alb-controller/variables.tf
+++ alb-controller/variables.tf
@@ -6,3 +6,13 @@ variable "cluster_name" {
   default = "k8s-demo" # VPC의 CIDR 블록 설정
   type = string
 }
+
+variable "AWS_ACCESS_KEY_ID" {
+  description = "AWS Access Key Id"
+  type    = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "AWS Secret Access Key"
+  type    = string
+}
