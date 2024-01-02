ba8a3b6a5b6db9031b366f2ebca31b34e97e62cd
odark <odark@naver.com>
2024-01-02 10:52:53 +0900

    variable update2

--- aws-nth/main.tf
+++ aws-nth/main.tf
@@ -34,7 +34,7 @@ resource "aws_sqs_queue" "terraform_queue" {
   name                      = var.queue_name
   delay_seconds             = 10
   max_message_size          = 2048
-  message_retention_seconds = 30
+  message_retention_seconds = 60
   receive_wait_time_seconds = 10
   redrive_policy = jsonencode({
     deadLetterTargetArn = aws_sqs_queue.terraform_queue_deadletter.arn
